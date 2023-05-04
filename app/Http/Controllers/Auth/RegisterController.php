4c530306a78bc1aa51293a8436b5a8a19a3ca8c5
FadlyFadilah <fadlyyusup22@gmail.com>
2023-05-04 07:44:56 +0700

    register di controller bukan di vendor

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -22,7 +23,7 @@ class RegisterController extends Controller
     |
     */
 
-    use RegistersUsers;
+    // use RegistersUsers;
 
     /**
      * Where to redirect users after registration.
@@ -47,6 +48,12 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
+    public function showRegistrationForm()
+    {
+        $roles = Role::whereNot('id', '1')->get();
+        return view('auth.register', compact('roles'));
+    }
+    
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -56,20 +63,20 @@ class RegisterController extends Controller
         ]);
     }
 
-         /**
-          * Create a new user instance after a valid registration.
-          *
-          * @param  array  $data
-          * @return \App\User
-          */
-         protected function create(array $data)
-         {
-            $user = User::create([
-                'name'     => $data['name'],
-                'username'    => $data['username'],
-                'password' => Hash::make($data['password']),
-            ]);
-            $user->roles()->sync(request()->input('roles'));
-            return $user;
-         }
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        $user = User::create([
+            'name'     => $data['name'],
+            'username'    => $data['username'],
+            'password' => Hash::make($data['password']),
+        ]);
+        $user->roles()->sync(request()->input('roles'));
+        return $user;
+    }
 }
