5db9e083153460a3d844049a2d0af2f235af8cf2
FadlyFadilah <fadlyyusup22@gmail.com>
2023-05-23 12:24:47 +0700

    userblade

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -127,8 +127,6 @@
     <a href="https://api.whatsapp.com/send?phone=6285163587878" class="float" target="_blank">
         <i class="fa fa-whatsapp my-float"></i>
     </a>
-    <script src="{{ asset('bs-stepper/js/bs-stepper.min.js') }}">
-    < script src = "https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js" >
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -151,7 +149,9 @@
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"></script>
     <script src="{{ asset('js/main.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
+    <script src="{{ asset('bs-stepper/js/bs-stepper.min.js') }}">
+    < script src = "https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js" >
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/moment/min/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
     <script>
