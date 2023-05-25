c88cc53c9900ec3fbe08cb10ebc4fc72cc6bc13f
FadlyFadilah <fadlyyusup22@gmail.com>
2023-05-25 15:00:30 +0700

    category

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -5,17 +5,24 @@
             <div class="hero-wrap js-fullheight">
                 <div class="background-overlay" style="background-image: url('{{ $cp->featured_image->getUrl() }}')"></div>
                 <div class="container">
-                    <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
+                    <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start"
+                        data-scrollax-parent="true">
                         <div class="col-md-7 ftco-animate">
                             <span class="strong">
                                 <a class="text-uppercase mr-2" href="https://www.cimahitechnopark.id/category/animasi">
-                                    <span style="border-left: 5px solid; padding-left: 5px;">Animasi</span>
+                                    @foreach ($cp->categories as $categori)
+                                        <span
+                                            style="border-left: 5px solid; padding-left: 5px;">{{ $categori->name }}</span>
+                                    @endforeach
                                 </a>
                             </span>
-                            <h1 class="display-4 text-white" style="font-family: 'Plus Jakarta Sans', sans-serif; font-weight: 600;">{{ $cp->title }}</h1>
+                            <h1 class="display-4 text-white"
+                                style="font-family: 'Plus Jakarta Sans', sans-serif; font-weight: 600;">{{ $cp->title }}
+                            </h1>
 
                             <p class="lead">
-                                <a class="btn btn-primary btn-lg" href="{{ route('news.show', $cp->title) }}" role="button">Lihat Lebih Banyak</a>
+                                <a class="btn btn-primary btn-lg" href="{{ route('news.show', $cp->title) }}"
+                                    role="button">Lihat Lebih Banyak</a>
                             </p>
                         </div>
                     </div>
@@ -26,32 +33,31 @@
 
 
 
-        <section class="ftco-section bg-light" style="background-image: url('vendor/technext/vacation-rental/images/bg_1.jpg');"
-            data-stellar-background-ratio="0.5">
-            <div class="overlay"></div>
-            <div class="bg-light">
-                <div class="container">
-                    <div class="row align-items-center">
-                        @foreach ($contentPages as $cpa)
-                            <div class="col-md-6 col-lg-3 mt-4">
-                                <div class="card" style="width: 18rem;" style="height: 500px;">
-                                    <img class="card-img-top"
-                                        src="{{ $cpa->featured_image->getUrl() }}"
-                                        alt="Card image cap">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{ $cpa->title }}</h5>
-                                        <p class="card-text">{!! substr($cpa->page_text, 0, 100) . '...' !!}</p>
-                                        <a href="{{ route('news.show', $cp->title) }}" class="btn btn-link">Lihat Lebih Banyak</a>
-                                    </div>
+    <section class="ftco-section bg-light" style="background-image: url('vendor/technext/vacation-rental/images/bg_1.jpg');"
+        data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="bg-light">
+            <div class="container">
+                <div class="row align-items-center">
+                    @foreach ($contentPagesAll as $cpa)
+                        <div class="col-md-6 col-lg-3 mt-4">
+                            <div class="card" style="width: 18rem;" style="height: 500px;">
+                                <img class="card-img-top" src="{{ $cpa->featured_image->getUrl() }}" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $cpa->title }}</h5>
+                                    <p class="card-text">{!! substr($cpa->page_text, 0, 100) . '...' !!}</p>
+                                    <a href="{{ route('news.show', $cpa->title) }}" class="btn btn-link">Lihat Lebih
+                                        Banyak</a>
                                 </div>
                             </div>
-                        @endforeach
-                    </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
-        </section>
-    @endsection
-    
+        </div>
+    </section>
+@endsection
+
 
 
 @section('scripts')
