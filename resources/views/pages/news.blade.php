d87c81fd631888233d872b8b56b81f3afc5b5e76
FadlyFadilah <fadlyyusup22@gmail.com>
2023-05-25 15:03:04 +0700

    berita

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -55,14 +55,14 @@
         <div class="bg-light">
             <div class="container">
                 <div class="row align-items-stretch">
-                    @foreach ($contentPages as $cpa)
+                    @foreach ($contentPagesAll as $cpa)
                         <div class="col-md-6 col-lg-3 mt-4">
                             <div class="card h-100 mx-auto" style="width: 18rem;">
                                 <img class="card-img-top" src="{{ $cpa->featured_image->getUrl() }}" alt="Card image cap" style="height: 200px;">
                                 <div class="card-body d-flex flex-column">
                                     <h5 class="card-title">{{ $cpa->title }}</h5>
                                     <p class="card-text">{!! substr($cpa->page_text, 0, 100) . '...' !!}</p>
-                                    <a href="{{ route('news.show', $cp->title) }}" class="btn btn-link mt-auto">Lihat Lebih Banyak</a>
+                                    <a href="{{ route('news.show', $cpa->title) }}" class="btn btn-link mt-auto">Lihat Lebih Banyak</a>
 >>>>>>> 659b857c6566b804be7691aabe1966df30d81294
                                 </div>
                             </div>
