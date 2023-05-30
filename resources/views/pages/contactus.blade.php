42b1bc81cd3d4109bf9dfb54d71ae2f46f55f757
TrisnaBudi <trisnabudi1411@gmail.com>
2023-05-30 10:59:31 +0700

    ui

--- resources/views/pages/contactus.blade.php
+++ resources/views/pages/contactus.blade.php
@@ -14,7 +14,6 @@
 </div>
 
 <section class="ftco-section bg-light" style="background-image: url('vendor/technext/vacation-rental/images/bg_1.jpg');" data-stellar-background-ratio="0.5">
-    <div class="overlay"></div>
     <div class="container">
         <div class="row">
             <div class="col-md-6">
